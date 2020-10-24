7dc65299b32e0806dcd6c07bc47f9a3e6c54d538
Thyraellis Howard <thyrahoward@outlook.com>
2020-10-24 17:04:19 -0400

    trying another post process

--- Assests/js/Retrieve_Data/POST_Users.js
+++ Assests/js/Retrieve_Data/POST_Users.js
@@ -7,28 +7,13 @@ $("#submitUser").on({
 })
 
 function sendJSON(){                
-    $.ajax({ 
-        url: "https://project-resourcing.github.io/Resource-Sync/Assests/js/Local_Data/users.json", 
-        type: "POST", 
-        data: {name: "Cisco", age: "Unknown", secretIdentity: "Francisco", powers: ["Dominating Code"]}, 
-        dataType: "json", 
-        success: function (result) { 
-            switch (result) { 
-                case true: 
-                    processResponse(result); 
-                    break; 
-                default: 
-                    resultDiv.html(result); 
-            } 
-        }, 
-        error: function (xhr, ajaxOptions, thrownError) { 
-        alert(xhr.status); 
-        alert("error: " + thrownError); 
-        } 
-    }); 
-
-    // Converting JSON data to string 
-    var data = JSON.stringify(
- 
-    ); 
+    fetch('https://project-resourcing.github.io/Resource-Sync/Assests/js/Local_Data/users.json', {
+      method: 'post',
+      body: {
+        name: "Cisco",
+        age: "Unknown",
+        secretIdentity: "Francisco",
+        powers: ["Dominating Code"]
+      }
+    })
 }
