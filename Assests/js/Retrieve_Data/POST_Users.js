4cd059cd29c844cd311329b91de390bc2bae3bab
Thyraellis Howard <thyrahoward@outlook.com>
2020-10-24 16:49:09 -0400

    changing post

--- Assests/js/Retrieve_Data/POST_Users.js
+++ Assests/js/Retrieve_Data/POST_Users.js
@@ -1,48 +1,34 @@
 
 $("#submitUser").on({
     click: function(){
+        alert("In");
         sendJSON();
     }
 })
 
 function sendJSON(){                
-    let result = document.querySelector('.result'); 
-    let name = document.querySelector('#name'); 
-    let email = document.querySelector('#email'); 
-        
-    // Creating a XHR object 
-    let xhr = new XMLHttpRequest(); 
-    let postURL = 'https://project-resourcing.github.io/Resource-Sync/Assests/js/Local_Data/users.json';
-
-
-    // open a connection 
-    xhr.open("POST", postURL, true); 
-
-    // Set the request header i.e. which type of content you are sending 
-    xhr.setRequestHeader("Content-Type", "application/json"); 
-
-    // Create a state change callback 
-    xhr.onreadystatechange = function () { 
-        if (xhr.readyState === 4 && xhr.status === 200) { 
-
-            // Print received data from server 
-            result.innerHTML = this.responseText; 
-
+    $.ajax({ 
+        url: "https://project-resourcing.github.io/Resource-Sync/Assests/js/Local_Data/users.json", 
+        type: "POST", 
+        data: {name: "Cisco", age: "Unknown", secretIdentity: "Francisco", powers: ["Dominating Code"]}, 
+        dataType: "json", 
+        success: function (result) { 
+            switch (result) { 
+                case true: 
+                    processResponse(result); 
+                    break; 
+                default: 
+                    resultDiv.html(result); 
+            } 
+        }, 
+        error: function (xhr, ajaxOptions, thrownError) { 
+        alert(xhr.status); 
+        alert("error: " + thrownError); 
         } 
-    }; 
+    }); 
 
     // Converting JSON data to string 
     var data = JSON.stringify(
-        {
-            "name": "Cisco",
-            "age": "Unknown",
-            "secretIdentity": "Francisco",
-            "powers": [
-                "Dominating Code"
-            ]
-        },
+ 
     ); 
-
-    // Sending data with the request 
-    xhr.send(data); 
 }
